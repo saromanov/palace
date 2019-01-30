cf41452794e0860bf9bd44f384ae4d5e23c01bce
Sergey <xxsmotur@gmail.com>
2019-01-30 18:36:22 +0500

    init for proxy agent

---
+++ pkg/proxy/agent.go
@@ -0,0 +1,16 @@
+package proxy
+
+import (
+	"context"
+)
+// Agent provides definition for agent proxy
+type Agent struct {
+
+}
+
+// Run provides starting of agent
+func (a*Agent) Run(ctx context.Context) {
+	for {
+		
+	}
+}
